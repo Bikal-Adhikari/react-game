c8940cf931d9f4b5d3cc102723d2120228667a5c
Bikal <bikaladhikari5@gmail.com>
2024-04-05 17:36:15 +1100

    Player component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import Header from "./components/Header";
+import Player from "./components/Player";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+      </div>
+    </>
+  );
 }
 
 export default App;
