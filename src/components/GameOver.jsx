76ddd99550d57d6ff3b1d6c6fa7fb4f60324b06f
Bikal <bikaladhikari5@gmail.com>
2024-04-06 16:22:07 +1100

    content for gameover added

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const GameOver = ({ winner }) => {
   return (
-    <div id="game-Over">
+    <div id="game-over">
       <h2>Game Over!</h2>
       <p>{winner} won!</p>
       <p>
